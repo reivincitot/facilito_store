b93631d7cf91dd71391348940d55a53a81fec7f8
reivincitot <reivincitot@gmail.com>
2023-01-10 19:02:41 -0300

    Video autenticacion modulo curso codigo facilito

--- facilito_store/urls.py
+++ facilito_store/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('usuarios/login', views.login_view, name='login'),
     path('admin/', admin.site.urls),
 ]
--- facilito_store/views.py
+++ facilito_store/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 
 def index(request):
     return render(request,'index.html',{
+        'message': 'Listado de productos',
+        'title': 'Productos',
+        'products':[
+            {'title': 'Playera','price':5,'stock': True},
+            {'title': 'Camisa','price':7,'stock': True},
+            {'title': 'Mochila','price':20,'stock': True},
+            {'title': 'Mitones','price':10,'stock': False}
+            ]
+    })
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request,user)
+            print('Usuario autenticado')
+        else:
+            print('Usuario no autenticado')
+        
+    return render(request,'users/login.html',{
         
     })
