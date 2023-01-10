eaea3c0a8defb9b46eab2ad35a13ffed0661acb5
reivincitot <reivincitot@gmail.com>
2023-01-10 14:14:46 -0300

    instalando bootstap5

--- facilito_store/settings.py
+++ facilito_store/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'facilito_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
