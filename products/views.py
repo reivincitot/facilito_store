8ef2b83673db61b453434927c32666d58fd3b22c
reivincitot <reivincitot@gmail.com>
2023-01-13 02:44:15 -0300

    Video claselistve modulo 4 curso django aplicacion web

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Product
 
-# Create your views here.
+class ProductListView(ListView):
+    template_name = 'index.html'
+    queryset = Product.objects.all().order_by('-id')
+    
+    def get_context_data(self, **kwargs):
+        contex = super().get_context_data(**kwargs)
+        contex['message']='Listado de productos'
+        print(contex)
+        return contex
