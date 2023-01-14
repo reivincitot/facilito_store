1bb2418981bf83c5af1cc7cd2bedee6c802204df
reivincitot <reivincitot@gmail.com>
2023-01-13 21:35:40 -0300

    Video 9 modulo 4 curso de aplicacion web con django codigo facilito

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Product(models.Model):
     title = models.CharField(max_length=50)
@@ -6,5 +7,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=2, default=0.0)
     slug = models.SlugField(null=False,blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    def save(self,*args, **kwargs):
+        self.slug = slugify(self.title)
+        return super(Product,self).save(*args, **kwargs)
     def __str__(self) :
         return self.title
