354f129bb45c5fa1a93014ab843ff0100b3f7f22
reivincitot <reivincitot@gmail.com>
2023-01-13 21:09:26 -0300

    Video slugs modulo 4 curso aplicaciones web django codigo facilito

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=8,decimal_places=2, default=0.0)
+    slug = models.SlugField(null=False,blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self) :
         return self.title
--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<pk>', views.ProductDetailView.as_view(), name='product') #id ->llave primaria
+    path('<slug:slug>', views.ProductDetailView.as_view(), name='product') #id ->llave primaria
 ]
