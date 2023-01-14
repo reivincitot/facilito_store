df689ec994f97218e449cd6cd0df625a3679895a
reivincitot <reivincitot@gmail.com>
2023-01-13 22:05:52 -0300

    Video 11 modulo 4 curso aplicaciones web codigo facilito

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
@@ -8,12 +9,20 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=2, default=0.0)
     slug = models.SlugField(null=False,blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    def save(self,*args, **kwargs):
-        self.slug = slugify(self.title)
-        return super(Product,self).save(*args, **kwargs)
+    # def save(self,*args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super(Product,self).save(*args, **kwargs)
     def __str__(self) :
         return self.title
+
 def set_slug(sender,instance,*args, **kwargs):
-    instance.slug = slugify(instance.title)
+    if instance.title and not instance.slug:
+        slug = slugify(instance.title)
+        while Product.objects.filter(slug=slug).exists():
+            slug = slugify(
+                '{}-{}'.format(instance.title,str(uuid.uuid4())[:8]) 
+            )
+        instance.slug=slug
+     
     
 pre_save.connect(set_slug, sender=Product)
