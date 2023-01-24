4f655a5190536797427a219b7ccd46987480edb6
reivincitot <reivincitot@gmail.com>
2023-01-24 16:52:25 -0300

    video 4 crear carrito de compras modulo 6 curso aplicaciones web con django codigo facilito

--- carts/views.py
+++ carts/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
+from .models import Cart
+
+
+
 
 # Create your views here.
 def cart(request):
-    #crear una sesion
-    #request.session['cart_id'] = '123' # la sesion es un diccionario 
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
     
-    #obtener el valor de una sesion
-    valor = request.session.get('cart_id')
-    print(valor)
     
-    #eliminar una sesion
-    request.session['cart_id'] = None
+    
+    
+    if request.session.get('cart_id'):
+        cart = Cart.objects.get(pk=cart_id)     #obtener el carrito de la base de datos
+    else:
+        cart = Cart.objects.create(user=user)
+     
+    request.session['cart_id'] = cart.id
     
     return render(request,'carts/cart.html',{
         
