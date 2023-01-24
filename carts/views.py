a3a75ff1c9185a5aa5f394df78c4d50aed2e1b90
reivincitot <reivincitot@gmail.com>
2023-01-24 16:40:00 -0300

    Vide 3 sesiones modulo 6 curso de aplicaciones web con django codigo facilito

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart(request):
+    #crear una sesion
+    #request.session['cart_id'] = '123' # la sesion es un diccionario 
+    
+    #obtener el valor de una sesion
+    valor = request.session.get('cart_id')
+    print(valor)
+    
+    #eliminar una sesion
+    request.session['cart_id'] = None
+    
     return render(request,'carts/cart.html',{
         
     })
