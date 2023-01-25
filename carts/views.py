314615603e4fecc25b54ea87cb8731547fd5e6a5
reivincitot <reivincitot@gmail.com>
2023-01-25 00:29:57 -0300

    Video 6 obtener o crear carrito de compras modulo 6 aplicaciones web con django codigo facilito
    creacion del archivo utils.py donde va la funciona cart

--- carts/views.py
+++ carts/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
 from .models import Cart
+from .utils import get_or_create_cart
 
 # Create your views here.
 def cart(request):
+    cart = get_or_create_cart(request)
     
-    user = request.user if request.user.is_authenticated else None
-    cart_id = request.session.get('cart_id')
-    
-    if cart_id:
-        cart = Cart.objects.get(cart_id=cart_id)     
-    else:
-        cart = Cart.objects.create(user=user)
-     
-    request.session['cart_id'] = cart.cart_id
     
     return render(request,'carts/cart.html',{
         
