4579c476476f669eaec076a0836443ef6044ec06
reivincitot <reivincitot@gmail.com>
2023-01-25 03:09:31 -0300

    Video 7 agregar productos al carrito modulo 6 curso aplicaciones web django codigo facilito

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Cart
 from .utils import get_or_create_cart
+from products.models import Product
+
 
 # Create your views here.
 def cart(request):
@@ -10,3 +12,13 @@ def cart(request):
     return render(request,'carts/cart.html',{
         
     })
+    
+def add(request):
+   cart = get_or_create_cart(request)
+   product = Product.objects.get(pk=request.POST.get('product_id'))
+   
+   cart.products.add(product)
+   
+   return render(request, 'carts/add.html',{
+       'product': product
+   })
