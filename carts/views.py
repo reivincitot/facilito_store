3f967acc0ea2eb51ac52ad8912909cf71fc4ea5c
reivincitot <reivincitot@gmail.com>
2023-01-26 13:53:35 -0300

    Video 11 Error 404 modulo 6 curso aplicaciones web con django codigo facilito

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,8 @@ from .models import Cart
 from .utils import get_or_create_cart
 from products.models import Product
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 def cart(request):
@@ -24,6 +26,7 @@ def add(request):
    })
    
 def remove(request):
+    
     cart = get_or_create_cart(request)
     product = Product.objects.get(pk=request.POST.get('product_id'))
     
