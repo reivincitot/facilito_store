cb1f7036f39982fcf126028deec4113f0841f0de
reivincitot <reivincitot@gmail.com>
2023-01-27 01:45:17 -0300

    Video 15 formulario cantidad modulo 6 curso apliaciones web con django curso codigo facilito

--- carts/models.py
+++ carts/models.py
@@ -10,7 +10,7 @@ from django.db.models.signals import m2m_changed
 class Cart(models.Model):
     cart_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product, through='CartProducts')
     subtotal = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     total= models.DecimalField(default=0, max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -30,7 +30,12 @@ class Cart(models.Model):
     def update_total(self):
         self.total= self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE) )
         self.save()
-        
+      
+class CartProducts(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
     
 def set_cart_id(sender, instance, *args, **kwargs):
     if not instance.cart_id:
