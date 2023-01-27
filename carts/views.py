dd07d2e76ff551558d94423809d6735b2969907f
reivincitot <reivincitot@gmail.com>
2023-01-27 12:27:34 -0300

    video 18 actualizar cantidad modulo 6 curso apliaciones web con django codigo facilito
    Sub total no esta entregando el valor correcto al sumar la cantidad con el valor el registro devuelve como valor 0

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Cart
+from .models import CartProducts
 from .utils import get_or_create_cart
 from .models import Product
 from django.shortcuts import redirect
@@ -18,11 +18,15 @@ def cart(request):
 def add(request):
    cart = get_or_create_cart(request)
    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
-   quantity = request.POST.get('quantity',1)
+   quantity = int(request.POST.get('quantity',1))
    
-   cart.products.add(product,through_defaults={
-       'quantity': quantity
-   })
+#    cart.products.add(product,through_defaults={
+#        'quantity': quantity
+#    })
+
+   cart_product = CartProducts.objects.create_or_update_quantity(cart=cart, 
+                                                                 product=product, 
+                                                                 quantity=quantity)
    
    return render(request, 'carts/add.html',{
        'product': product
