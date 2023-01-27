d3076e262bfbea0ecdc63594b453dca67d48034b
reivincitot <reivincitot@gmail.com>
2023-01-27 03:02:34 -0300

    Video 17 Problema n+1 query modulo 6 curso aplicaciones web con django codigo facilito

--- carts/models.py
+++ carts/models.py
@@ -30,6 +30,9 @@ class Cart(models.Model):
     def update_total(self):
         self.total= self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE) )
         self.save()
+        
+    def products_related(self):
+        return self.cartproducts_set.select_related('product')
       
 class CartProducts(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
