ce145cc6bd7b463bd39909ed98d90767d3f778c2
reivincitot <reivincitot@gmail.com>
2023-01-27 00:55:57 -0300

    video 14 mostrar subtotal modulo 6 curso de apicaciones web con django codigo facilito
    correcion del lugar del uso de font weight bold y cambio al fornmato de bootstrap 5 agregado de boton seguir comprando para el template de productos

--- carts/models.py
+++ carts/models.py
@@ -1,8 +1,10 @@
 import uuid
+import decimal
 from django.db import models
 from users.models import User
 from products.models import Product
 from django.db.models.signals import pre_save
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 class Cart(models.Model):
@@ -12,14 +14,31 @@ class Cart(models.Model):
     subtotal = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     total= models.DecimalField(default=0, max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+    FEE = 0.05
     
     def __str__(self):
         return self.cart_id
+    
+    def update_totals(self):
+        self.update_subtotal()
+        self.update_total()
+        
+    def update_subtotal(self):
+        self.subtotal = sum([product.price for product in self.products.all()])
+        self.save()
+        
+    def update_total(self):
+        self.total= self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE) )
+        self.save()
+        
     
 def set_cart_id(sender, instance, *args, **kwargs):
     if not instance.cart_id:
         instance.cart_id = str(uuid.uuid4())
 
-
+def update_totals(sender,instance,action, *args, **kwargs):
+    if action == 'post_add' or action=='post_remove' or action=='post_clear':
+        instance.update_totals()
 
 pre_save.connect(set_cart_id, sender=Cart)
+m2m_changed.connect(update_totals,sender=Cart.products.through)
--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Cart
 from .utils import get_or_create_cart
-from products.models import Product
+from .models import Product
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 
@@ -17,7 +17,7 @@ def cart(request):
     
 def add(request):
    cart = get_or_create_cart(request)
-   product = Product.objects.get(pk=request.POST.get('product_id'))
+   product = get_object_or_404(Product, pk=request.POST.get('product_id'))
    
    cart.products.add(product)
    
@@ -26,9 +26,8 @@ def add(request):
    })
    
 def remove(request):
-    
     cart = get_or_create_cart(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
     
     cart.products.remove(product)
     
