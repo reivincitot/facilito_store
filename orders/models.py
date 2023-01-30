065876d30e3e541948deb974b290d790083f9f08
reivincitot <reivincitot@gmail.com>
2023-01-30 02:35:01 -0300

    Video 5 resumen del pedido modulo 7 curso aplicaciones web con django codigo facilito

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,11 @@
+import uuid
 from enum import Enum
 from django.db import models
 from users.models import User
 from carts.models import Cart
+from django.db.models.signals import pre_save
+#from django.db.models.signals import post_save
+
 
 
 
@@ -11,11 +15,12 @@ class OrderStatus(Enum):
     CREATED= 'CREATED'
     PAYED = 'PAYED'
     COMPLETED = 'COMPLETED'
-    CENCELED = ' CANCELED'
+    CANCELED = ' CANCELED'
     
 choices = [(tag, tag.value) for tag in OrderStatus]
 
 class Order(models.Model):
+    order_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     status = models.CharField(max_length=50, choices=choices, default=OrderStatus.CREATED)
@@ -25,4 +30,10 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return ''
+        return self.order_id
+    
+def set_order_id(sender,instance, *args, **kwargs):
+     if not instance.order_id:
+         instance.order_id = str(uuid.uuid4())
+
+pre_save.connect(set_order_id, sender=Order)
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .utils import get_or_create_order
+from carts.utils import get_or_create_cart
+from .models import Order
 
 # Create your views here.
-def order(request):
-    return render(request,'orders/order.html')
+def order(request):  
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(cart,request)
+      
+    return render(request,'orders/order.html',{
+        'cart':cart,
+        'order':order,
+    })
