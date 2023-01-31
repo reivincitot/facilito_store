32c17b5d09bcba2fad8e3daa2bcd4ec128ceee9c
reivincitot <reivincitot@gmail.com>
2023-01-31 02:03:21 -0300

    Video 8 login requerido modulo 7 curso aplicaciones web con django codigo facilito

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,11 @@
+from .models import Order
 from django.shortcuts import render
 from .utils import get_or_create_order
 from carts.utils import get_or_create_cart
-from .models import Order
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def order(request):  
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart,request)
