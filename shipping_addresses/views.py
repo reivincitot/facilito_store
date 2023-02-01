455cb86b5da58a0b322dee18f484a8b464d595f0
reivincitot <reivincitot@gmail.com>
2023-02-01 20:34:38 -0300

    Video 2 vista de direcciones modulo 8 curso aplicaciones web con django codigo facilito

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,3 +1,13 @@
+from .models import ShippingAddress
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
 
 # Create your views here.
+class ShippingAddressListView(ListView):
+    model = ShippingAddress()
+    template_name = 'shipping_addresses/shipping_addresses.html'
+    
+    def get_queryset(self):
+        return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
+    
