5c842093ed27fded6540037e48c8e6d91a7e85b9
reivincitot <reivincitot@gmail.com>
2023-02-02 01:24:06 -0300

    Video 5 crear direcciones de envio modulo 8 curso aplicaciones web con django codigo facilito

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,4 +1,6 @@
 from .models import ShippingAddress
+from django.shortcuts import redirect
+from django.contrib import messages
 from django.shortcuts import render
 from .forms import ShippingAddressForm
 from django.views.generic.list import ListView
@@ -13,7 +15,18 @@ class ShippingAddressListView(ListView):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
     
 def create(request):
-    form = ShippingAddressForm()
+    form = ShippingAddressForm(request.POST or None)
+    
+    if request.method == 'POST' and form.is_valid():
+        shipping_address = form.save(commit=False)
+        shipping_address.user = request.user
+        shipping_address.default = not ShippingAddress.objects.filter(user=request.user).exists()
+        
+        shipping_address.save()
+    
+        messages.success(request,'Dirección creada exitosamente')
+        return redirect('shipping_addresses:shipping_addresses')
+        
     return render(request, 'shipping_addresses/create.html',{
         'form':form
     })
