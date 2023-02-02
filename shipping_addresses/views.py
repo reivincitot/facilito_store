eed5bac1fac5fb27269a3b98407ca04aaf33ec40
reivincitot <reivincitot@gmail.com>
2023-02-02 02:30:13 -0300

    Video 7 restringir direcciones modulo 8 curso aplicaciones web con django codigo facilito
    En este video se restringe el acceso y la vista del apartado mis direcciones a las personas que no estan autenticadas

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -4,16 +4,20 @@ from django.contrib import messages
 from django.shortcuts import render
 from .forms import ShippingAddressForm
 from django.views.generic.list import ListView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-class ShippingAddressListView(ListView):
+class ShippingAddressListView(LoginRequiredMixin, ListView):
+    login_url='login'
     model = ShippingAddress
     template_name = 'shipping_addresses/shipping_addresses.html'
     
     def get_queryset(self):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
-    
+
+@login_required(login_url='login')   
 def create(request):
     form = ShippingAddressForm(request.POST or None)
     
