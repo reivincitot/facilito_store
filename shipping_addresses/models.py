b87af554f61cf6005d9bf01299e44f812af01a90
reivincitot <reivincitot@gmail.com>
2023-02-02 02:01:54 -0300

    Video 6 lista de direcciones modulo 8 curso de aplicaciones web con django codigo facilito

--- shipping_addresses/models.py
+++ shipping_addresses/models.py
@@ -16,3 +16,6 @@ class ShippingAddress(models.Model):
     def __str__(self):
         return self.postal_code
     
+    @property
+    def address(self):
+        return '{}-{}-{}'.format(self.city,self.state,self.country)
