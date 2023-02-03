3cadcde2fcd5bdec2374471209fcc549544bbc01
reivincitot <reivincitot@gmail.com>
2023-02-03 04:06:12 -0300

    Video 11 direccion principal modulo 8 curso aplicaciones web con django codigo facilito

--- shipping_addresses/urls.py
+++ shipping_addresses/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.ShippingAddressListView.as_view(), name='shipping_addresses'),
     path('nuevo',views.create,name='create'),
     path('editar/<int:pk>', views.ShippingAddressUpdateView.as_view(), name='update'),
-    path('eliminar/int:<int:pk>',views.ShippingAddressDeleteView.as_view(),name='delete')
+    path('eliminar/int:<int:pk>',views.ShippingAddressDeleteView.as_view(),name='delete'),
+    path('default/<int:pk>', views.default,name='default')
 ]
