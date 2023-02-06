a24edb47365684f04ff932288d07d90da1140d25
reivincitot <reivincitot@gmail.com>
2023-02-06 01:28:24 -0300

    Video 2 relacion orden y direccion modulo 9 curso aplicaciones web con django codigo facilito

--- orders/views.py
+++ orders/views.py
@@ -22,10 +22,11 @@ def address(request):
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart, request)
     
-    shipping_address=order.shiping_address
+    shipping_address=order.get_or_set_shipping_address()
     
     return render(request,'orders/address.html', {
         'cart':cart,
         'order':order,
+        'shipping_address':shipping_address,
         'breadcrumb': breadcrumb(address=True)
     })
