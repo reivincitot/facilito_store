a4f7d4428cd3ac67559033c268c5e2d77fc71ccd
reivincitot <reivincitot@gmail.com>
2023-02-07 02:15:46 -0300

    video 4 seleccionar direccion modulo 9 curso aplicaciones web con django codigo facilito

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,6 @@ app_name = 'orders'
 urlpatterns = [
     path('',views.order, name='order'),
     path('direccion', views.address, name='address'),
+    path('seleccionar/direccion',views.select_address, name='select_address'),
+    path('establecer/direccion/<int:pk>', views.check_address, name='check_address')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,13 @@
 from .models import Order
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from .utils import get_or_create_order
 from .utils import breadcrumb
 from carts.utils import get_or_create_cart
 from django.contrib.auth.decorators import login_required
+
+from shipping_addresses.models import ShippingAddress
 
 # Create your views here.
 @login_required(login_url='login')
@@ -22,11 +26,34 @@ def address(request):
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart,request)
     
-    shipping_adrress = order.get_or_set_shipping_address()
+    shipping_address = order.get_or_set_shipping_address()
         
     return render(request,'orders/address.html',{
         'cart': cart,
         'order': order,
-        'shipping_address': shipping_adrress,
+        'shipping_address': shipping_address,
         'breadcrumb': breadcrumb(address=True)
     })
+    
+@login_required(login_url='login')
+def select_address(request):
+    shipping_addresses = request.user.shippingaddress_set.all()
+    
+    return render(request,'orders/select_address.html',{
+        'breadcrumb': breadcrumb(address=True),
+        'shipping_addresses': shipping_addresses
+    })
+
+@login_required(login_url='login')    
+def check_address(request,pk):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(cart,request)
+    
+    shipping_address = get_object_or_404(ShippingAddress, pk=pk)
+    
+    if request.user.id != shipping_address.user_id:
+        return redirect('carts:cart')
+    
+    order.update_shipping_address(shipping_address)
+    
+    return redirect('orders:address')
