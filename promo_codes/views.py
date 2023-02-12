b5baf5c58bad1d55e6ff8f452cb08d2f3c129016
reivincitot <reivincitot@gmail.com>
2023-02-11 22:37:09 -0300

    Video 4 peticion asincrona modulo 11 curso aplicaciones web con django codigo facilito

--- promo_codes/views.py
+++ promo_codes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def validate(requst):
+    return JsonResponse({
+        'name':'Eduardo',
+        'job':'codigofacilito'
+    })
