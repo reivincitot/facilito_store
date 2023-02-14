a66f3631aa1c8cfe3fc2db4dba739d3d8c1c43cd
reivincitot <reivincitot@gmail.com>
2023-02-14 15:29:45 -0300

    Video validar codigo de promoción moudulo 11 curso aplicaciones web con django codigo facilito

--- promo_codes/models.py
+++ promo_codes/models.py
@@ -1,9 +1,16 @@
-import string
 import random
+import string
 
 from django.db import models
 from django.db.models.signals import pre_save
+from django.utils import timezone
+
 # Create your models here.
+
+class PromoCodeManager(models.Manager):
+    def get_valid(self,code):
+        now = timezone.now()
+        return self.filter(code=code).filter(used=False).filter(valid_from__lte=now).filter(valid_to__gte=now).first()
 
 
 class PromoCode(models.Model):
@@ -14,11 +21,13 @@ class PromoCode(models.Model):
     used = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     
+    objects = PromoCodeManager()
+    
     def __str__(self):
         return self.code
 
     def use(self):
-        self.used = True
+        #self.used = True
         self.save()
     
 def set_code(sender,instance, *args, **kwargs):
